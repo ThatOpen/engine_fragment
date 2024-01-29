24629c6951f585702d3ca3266c1c932485e20f8f
Antonio González Viegas <antoniogviegas@hotmail.com>
2024-01-29 19:32:52 +0100

    chore: fix example scene colors

--- resources/simple-three-scene.js
+++ resources/simple-three-scene.js
@@ -2,12 +2,10 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import Stats from "stats.js/src/Stats.js";
 
-
 /**
  * Simple Three.js scene to avoid code repetition in each example
  */
 export class SimpleThreeScene {
-
   scene;
   size;
   camera;
@@ -21,39 +19,46 @@ export class SimpleThreeScene {
   }
 
   initializeScene(canvas) {
-    //Creates the Three.js scene
+    // Creates the Three.js scene
     this.scene = new THREE.Scene();
 
-    //Object to store the size of the viewport
+    // Object to store the size of the viewport
     this.size = {
       width: window.innerWidth,
       height: window.innerHeight,
     };
 
-    //Creates the camera (point of view of the user)
-    this.camera = new THREE.PerspectiveCamera(75, this.size.width / this.size.height);
+    // Creates the camera (point of view of the user)
+    this.camera = new THREE.PerspectiveCamera(
+      75,
+      this.size.width / this.size.height
+    );
     this.camera.position.z = 5;
     this.camera.position.y = 4;
     this.camera.position.x = 2;
 
-    //Creates the lights of the scene
+    // Creates the lights of the scene
     const lightColor = 0xffffff;
-    const ambientLight = new THREE.AmbientLight(lightColor, 0.5);
+    const ambientLight = new THREE.AmbientLight(lightColor, 1.5);
     this.scene.add(ambientLight);
 
-    const directionalLight = new THREE.DirectionalLight(lightColor, 1);
+    const directionalLight = new THREE.DirectionalLight(lightColor, 2);
     directionalLight.position.set(0, 10, 0);
     directionalLight.target.position.set(-5, 0, 0);
     this.scene.add(directionalLight);
     this.scene.add(directionalLight.target);
 
-    //Sets up the renderer, fetching the canvas of the HTML
+    // Sets up the renderer, fetching the canvas of the HTML
     this.canvas = canvas;
-    this.renderer = new THREE.WebGLRenderer({canvas: this.canvas, alpha: true});
+    this.renderer = new THREE.WebGLRenderer({
+      canvas: this.canvas,
+      alpha: true,
+    });
     this.renderer.setSize(this.size.width, this.size.height);
     this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    this.renderer.outputColorSpace = THREE.LinearSRGBColorSpace;
 
-    //Creates grids and axes in the scene
+    // Creates grids and axes in the scene
     const grid = new THREE.GridHelper(50, 30);
     this.scene.add(grid);
 
@@ -62,7 +67,7 @@ export class SimpleThreeScene {
     axes.renderOrder = 1;
     this.scene.add(axes);
 
-    //Creates the orbit controls (to navigate the scene)
+    // Creates the orbit controls (to navigate the scene)
     this.controls = new OrbitControls(this.camera, this.canvas);
     this.controls.enableDamping = true;
     this.controls.target.set(-2, 0, 0);
@@ -72,7 +77,7 @@ export class SimpleThreeScene {
     this.stats.showPanel(2);
     document.body.append(this.stats.dom);
 
-    //Animation loop
+    // Animation loop
     const animate = () => {
       this.stats.begin();
       this.controls.update();
@@ -83,7 +88,7 @@ export class SimpleThreeScene {
 
     animate();
 
-    //Adjust the viewport to the size of the browser
+    // Adjust the viewport to the size of the browser
     window.addEventListener("resize", () => {
       this.size.width = window.innerWidth;
       this.size.height = window.innerHeight;
