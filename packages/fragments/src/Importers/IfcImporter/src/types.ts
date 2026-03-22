26e114d423c611fc92a69a21de51d1a5d7947f0b
ShaMan123 <shacharnen@gmail.com>
2026-03-22 10:58:27 +0200

    jsdoc example

--- packages/fragments/src/Importers/IfcImporter/src/types.ts
+++ packages/fragments/src/Importers/IfcImporter/src/types.ts
@@ -16,7 +16,22 @@ export interface ProcessData {
    */
   readFromCallback?: boolean;
   /**
-   * Read ifc file incrementally, use instead of passing {@link bytes}
+   * Read ifc file incrementally, instead of passing {@link bytes}.
+   *
+   * @example node.js
+   * ```typescript
+   * import { open } from "node:fs/promises";
+   *
+   * const handle = await open(filePath, "r");
+   * const chunkSize = 64 * 1024; // 64KB
+   * const buffer = new Uint8Array(chunkSize);
+   * const readCallback: ((offset: number) => {
+   *   const bytesRead = readSync(handle.fd, buffer, 0, chunkSize, offset);
+   *   return buffer.slice(0, bytesRead);
+   * })
+   * const output = await importer.process({ readFromCallback: true, readCallback });
+   * await handle.close();
+   * ```
    */
   readCallback?: ModelLoadCallback;
   raw?: boolean;
