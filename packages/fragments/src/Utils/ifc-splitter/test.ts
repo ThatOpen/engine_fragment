ab05fb934d759d71ad96919e438d06d23ee86bb9
ShaMan123 <shacharnen@gmail.com>
2026-04-30 16:51:38 +0300

    node bindings

--- packages/fragments/src/Utils/ifc-splitter/test.ts
+++ packages/fragments/src/Utils/ifc-splitter/test.ts
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
-import * as fs from "fs";
 import * as path from "path";
-import { split } from "./index";
+import { IfcSplitterNode } from "./node";
 
 const args = process.argv.slice(2);
 if (args.length < 2) {
@@ -12,10 +11,14 @@ if (args.length < 2) {
 
 const inputPath = path.resolve(args[0]);
 const numGroups = parseInt(args[1], 10);
-const outputDir = args[2] ? path.resolve(args[2]) : undefined;
+const outputDir = args[2] ? path.resolve(args[2]) : path.dirname(inputPath);
 
 try {
-  const splitMap = split({ fs, path }, inputPath, numGroups, outputDir);
+  const splitMap = await new IfcSplitterNode().split(
+    inputPath,
+    numGroups,
+    (groupId) => path.resolve(outputDir, `${groupId + 1}.ifc`),
+  );
   console.log(splitMap);
 } catch (err) {
   console.error(err);
