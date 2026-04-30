cf0266f6c8e29bdd71645440c8bfc21c15fec3ae
ShaMan123 <shacharnen@gmail.com>
2026-04-30 17:26:08 +0300

    fix(): async iterator (should be supported)

--- packages/fragments/src/Utils/ifc-splitter/node.ts
+++ packages/fragments/src/Utils/ifc-splitter/node.ts
@@ -2,12 +2,16 @@
 import { openAsBlob } from "fs";
 import { open } from "node:fs/promises";
 import { Writable } from "node:stream";
-import { IfcSplitter } from "./index";
+import { TextDecoderStream } from "node:stream/web";
+import { IfcSplitter } from "./index.ts";
 
 export class IfcSplitterNode extends IfcSplitter {
   constructor() {
     super({
-      readableStream: async (path) => (await openAsBlob(path)).stream(),
+      readableStream: async (path) =>
+        (await openAsBlob(path, { type: "text/plain" }))
+          .stream()
+          .pipeThrough(new TextDecoderStream()),
       writableStream: async (path) => {
         const fileHandle = await open(path, "w");
         const nodeWritable = fileHandle.createWriteStream();
