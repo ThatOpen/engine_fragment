453f09ab294f4cc08a4a1c3327b829c7f9852710
ShaMan123 <shacharnen@gmail.com>
2026-04-30 16:51:38 +0300

    node bindings

---
+++ packages/fragments/src/Utils/ifc-splitter/node.ts
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+import { openAsBlob } from "fs";
+import { open } from "node:fs/promises";
+import { Writable } from "node:stream";
+import { IfcSplitter } from "./index";
+
+export class IfcSplitterNode extends IfcSplitter {
+  constructor() {
+    super({
+      readableStream: async (path) => (await openAsBlob(path)).stream(),
+      writableStream: async (path) => {
+        const fileHandle = await open(path, "w");
+        const nodeWritable = fileHandle.createWriteStream();
+        return Writable.toWeb(nodeWritable);
+      },
+    });
+  }
+}
