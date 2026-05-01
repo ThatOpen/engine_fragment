e8995f1d9cb17f4fc58eb159617fb6895851151c
ShaMan123 <shacharnen@gmail.com>
2026-05-01 08:44:08 +0300

    refactor(forEachLine): IfcDecoderStream

---
+++ packages/fragments/src/Utils/ifc-stream.ts
@@ -0,0 +1,71 @@
+const cr = "\r";
+const nl = "\n";
+
+/**
+
+ *
+ * @example
+ * ```ts
+ * let blob: Blob;
+ * 
+ * const ifcStream = blob
+ *   .stream()
+ *   .pipeThrough(new IfcDecoderStream());
+ * 
+ * for await (const line of ifcStream) {
+ *   // parse line
+ * }
+ * ```
+ * 
+ * @example node
+ * ```ts
+ * let blob = await fs.openAsBlob(path, { type: "text/plain" });
+ * ```
+ */
+export class IfcDecoderStream extends TransformStream<Uint8Array, string> {
+  constructor(encoding = "utf-8") {
+    let tail = "";
+    const decoder = new TextDecoder(encoding);
+
+    super({
+      transform(chunk, controller) {
+        const text = decoder.decode(chunk, { stream: true });
+        if (!text) return;
+        let start = 0;
+        let idx = text.indexOf(nl);
+
+        if (idx !== -1) {
+          let end = idx;
+          if (end > 0 && text.charAt(end - 1) === cr) end--;
+          controller.enqueue(
+            tail
+              ? tail + text.substring(start, end)
+              : text.substring(start, end),
+          );
+          tail = "";
+          start = idx + 1;
+          idx = text.indexOf(nl, start);
+        } else {
+          tail += text;
+          return;
+        }
+
+        while (idx !== -1) {
+          let end = idx;
+          if (end > start && text.charAt(end - 1) === cr) end--;
+          controller.enqueue(text.substring(start, end));
+          start = idx + 1;
+          idx = text.indexOf(nl, start);
+        }
+
+        if (start < text.length) tail = text.substring(start);
+      },
+
+      flush(controller) {
+        const remaining = decoder.decode();
+        const full = tail + remaining;
+        if (full) controller.enqueue(full);
+      },
+    });
+  }
+}
