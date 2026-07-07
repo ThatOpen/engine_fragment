be1831169e453d6dd21e77718054de1aa6f62d52
ShaMan123 <shacharnen@gmail.com>
2026-07-07 15:20:03 +0300

    expose interface

--- packages/fragments/src/FragmentsModels/src/index.ts
+++ packages/fragments/src/FragmentsModels/src/index.ts
@@ -1,4 +1,5 @@
 export * from "./model/model-types";
+export type { IFragmentsModel } from "./model/fragments-model-interface";
 export { FragmentsModel } from "./model";
 export { SingleThreadedFragmentsModel } from "./single-threading";
 export { isRawBuffer } from "./utils/misc/buffer";
